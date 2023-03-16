627664af1088f471f4b73f60a5f51dde4e6405fb
Simon Pasquier <spasquie@redhat.com>
2023-03-16 17:09:31 +0100

    Add script for deploying a local setup
    The hack/ directory contains scripts and manifests to provision a
    metrics+logs stack on a Kind cluster for local testing.
    
    The `hack/setup.sh` script can be run to deploy:
    * the kube-prometheus stack for metrics and alerts.
    * Loki+promtail for logs.
    
    The `hack/jsonnet` directory contains Jsonnet files using the
    kube-prometheus libraries.
    
    The hack/generate.sh script can be used to regenerate the manifests in
    hack/manifests after changes to the kube-prometheus version.
    
    Signed-off-by: Simon Pasquier <spasquie@redhat.com>

---
+++ hack/setup.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+# Setup a kind cluster with all korrel8r signals
+set -e -u -o pipefail
+
+SCRIPT_PATH=$(readlink -f "$0")
+SCRIPT_DIR=$(cd "$(dirname "$SCRIPT_PATH")" && pwd)
+
+wait_ready() {
+	kubectl wait --for=condition=Ready pods --all --all-namespaces --timeout=300s
+}
+
+create_kind() {
+	kind create cluster --config="${SCRIPT_DIR}/kind_config.yaml"
+	wait_ready
+}
+
+create_ingress() {
+	helm repo add traefik https://traefik.github.io/charts
+	helm repo update
+	kubectl create namespace traefik
+	cat << EOF | helm install -f - --namespace traefik traefik traefik/traefik
+image:
+  name: traefik
+  pullPolicy: IfNotPresent
+service:
+  type: NodePort
+ports:
+  web:
+    nodePort: 30000
+  websecure:
+    nodePort: 30001
+nodeSelector:
+  ingress-ready: 'true'
+EOF
+	wait_ready
+}
+
+apply() {
+	kubectl apply --server-side --force-conflicts -f "${SCRIPT_DIR}/manifests/setup"
+	kubectl apply --server-side --force-conflicts -f "${SCRIPT_DIR}/manifests/"
+	wait_ready
+}
+
+case ${1:-"help"} in
+create)
+	create_kind
+	create_ingress
+	;;
+
+apply)
+	apply
+	;;
+
+help)
+	echo "usage: $(basename "$0") { create | apply }"
+	;;
+
+esac
