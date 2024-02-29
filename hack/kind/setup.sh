36b630acfdb755618876dcad2789bcfd6e1c473e
Alan Conway <aconway@redhat.com>
2024-02-29 14:59:28 -0500

    Minor improvements to deploy.sh
    Changes to make the script more reliable, fixes some race conditions.
    
    - added existence checks
    - added some missing resource types for cleanup
    - 2 space indenting.

--- hack/kind/setup.sh
+++ hack/kind/setup.sh
@@ -6,19 +6,19 @@ SCRIPT_PATH=$(readlink -f "$0")
 SCRIPT_DIR=$(cd "$(dirname "$SCRIPT_PATH")" && pwd)
 
 wait_ready() {
-	kubectl wait --for=condition=Ready pods --all --all-namespaces --timeout=300s
+    kubectl wait --for=condition=Ready pods --all --all-namespaces --timeout=300s
 }
 
 create_kind() {
-	kind create cluster --name "${CLUSTER_NAME:-korrel8r}" --config="${SCRIPT_DIR}/kind_config.yaml"
-	wait_ready
+    kind create cluster --name "${CLUSTER_NAME:-korrel8r}" --config="${SCRIPT_DIR}/kind_config.yaml"
+    wait_ready
 }
 
 create_ingress() {
-	helm repo add traefik https://traefik.github.io/charts
-	helm repo update
-	kubectl create namespace traefik
-	cat << EOF | helm install -f - --namespace traefik traefik traefik/traefik
+    helm repo add traefik https://traefik.github.io/charts
+    helm repo update
+    kubectl create namespace traefik
+    cat << EOF | helm install -f - --namespace traefik traefik traefik/traefik
 image:
   name: traefik
   pullPolicy: IfNotPresent
@@ -34,39 +34,39 @@ ports:
 nodeSelector:
   ingress-ready: 'true'
 EOF
-	wait_ready
+    wait_ready
 }
 
 create_dashboard() {
-       helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
-       cat <<EOF | helm install dashboard kubernetes-dashboard/kubernetes-dashboard -f - -n kubernetes-dashboard --create-namespace
+    helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+    cat <<EOF | helm install dashboard kubernetes-dashboard/kubernetes-dashboard -f - -n kubernetes-dashboard --create-namespace
 protocolHttp: true
 service:
   externalPort: 8080
 rbac:
   clusterReadOnlyRole: true
 EOF
-	wait_ready
+    wait_ready
 }
 
 apply() {
-	kubectl apply --server-side --force-conflicts -f "${SCRIPT_DIR}/manifests/setup"
-	kubectl apply --server-side --force-conflicts -f "${SCRIPT_DIR}/manifests/"
-	wait_ready
+    kubectl apply --server-side --force-conflicts -f "${SCRIPT_DIR}/manifests/setup"
+    kubectl apply --server-side --force-conflicts -f "${SCRIPT_DIR}/manifests/"
+    wait_ready
 }
 
 case ${1:-"help"} in
-create)
+    create)
 	create_kind
 	create_ingress
 	create_dashboard
 	;;
 
-apply)
+    apply)
 	apply
 	;;
 
-help)
+    help)
 	echo "usage: $(basename "$0") { create | apply }"
 	;;
 
