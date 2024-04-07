11d4a5989c146d697146ea0932c318c67bea5e62
Alan Conway <aconway@redhat.com>
2024-04-07 01:40:19 -0400

    Improved reliability of wait_for_csv script.
    Was getting tripped up by trying to get status.phase before the resource had a status.

--- hack/wait-rollout.sh
+++ hack/wait-rollout.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 n=0
-until oc get "$@"; do
+until oc get "$@" > /dev/null; do
   echo "waiting for resources: $*"
   sleep 5
   [ $(( ++n )) = 6 ] && { echo "timed out waiting for $*"; return 1; }
 done
-
+echo "waiting for rollout status: $*"
 oc rollout status --watch --timeout=5m "$@" || exit 1
