11fa8ac1848336c95f2e275c918a8cf7aa7fd66b
Alan Conway <aconway@redhat.com>
2024-07-31 19:47:08 -0400

    fix!: Update flags, breaking changes.
    - Removed deprecated `korrel8r web` command line flags `--http` and `--rest`
    - Removed `korrel8r --profile=<type>` flag, use `korrel8r web --profile`
    - Add `korrel8r web --profile` to enable/disable http profiles.
    - Hid `korrel8r --panic` flag, may remove later.

--- pkg/domains/metric/metric_test.go
+++
@@ -1,9 +0,0 @@
-// Copyright: This file is part of korrel8r, released under https://github.com/korrel8r/korrel8r/blob/main/LICENSE
-
-package metric_test
-
-import "testing"
-
-func TestFIXME(t *testing.T) {
-	t.Fatal("FIXME")
-}
