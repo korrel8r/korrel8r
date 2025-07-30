48ae6bd5ac80ef596ac313824f8457dcacff2593
Alan Conway <aconway@redhat.com>
2025-07-30 16:17:02 -0400

    refactor: k8s Query, engine storeHolder
    - k8s.Query separate the Selector class as a plain JSON object for re-use in other domains.
    - engine: rename store to storeHolder to clarify relationship with "real" stores.

--- internal/pkg/test/test.go
+++ internal/pkg/test/test.go
@@ -44,7 +44,7 @@ func SkipIfNoCluster(t testing.TB) {
 			return
 		}
 		ns := &corev1.Namespace{}
-		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 		defer cancel()
 		errCluster = c.Get(ctx, types.NamespacedName{Name: "default"}, ns)
 	})
