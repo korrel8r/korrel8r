9e75cc17f62574a5b59676310d023a1bc1c07166
Alan Conway <aconway@redhat.com>
2026-07-10 11:45:39 -0400

    refactor: separate request/response logs for MCP and HTTP

--- pkg/rest/logger.go
+++ pkg/rest/logger.go
@@ -10,48 +10,35 @@ import (
 
 // logger is a Gin handler to log requests for debugging.
 func (a *API) logger(c *gin.Context) {
-	if !log.V(2).Enabled() {
-		return // Nothing to do for V < 2
-	}
+	if log.V(3).Enabled() {
+		start := time.Now()
+		detail := log.V(9).Enabled() // Extra detail
+		common := []any{"method", c.Request.Method, "url", c.Request.URL}
+		if sn, _ := a.session(c); sn != nil {
+			common = append(common, "session", sn.ID)
+		}
 
-	start := time.Now()
-	detail := log.V(9).Enabled() // Extra detail
-
-	values := []any{
-		"method", c.Request.Method,
-		"url", c.Request.URL,
-	}
-	var rw *responseWriter
-	if detail {
-		rw = newResponseWriter(c.Writer)
-		c.Writer = rw // Save the response
-		values = append(values,
-			"from", c.Request.RemoteAddr,
-			"body", copyBody(c.Request),
-		)
-	}
-
-	log.V(3).Info("REST Request", values...)
-
-	defer func() {
-		values = append(values,
-			"status", c.Writer.Status(),
-			"latency", time.Since(start),
-		)
+		// Log on receiving request
+		values := common
+		var rw *responseWriter
 		if detail {
-			values = append(values,
-				"request", copyBody(c.Request),
-				"response", rw.String(),
-			)
+			rw = newResponseWriter(c.Writer)
+			c.Writer = rw // Save the response
+			values = append(values, "from", c.Request.RemoteAddr, "body", copyBody(c.Request))
 		}
-		if len(c.Errors.Errors()) > 0 {
-			values = append(values, "errors", c.Errors.Errors())
-		}
-		if c.IsAborted() || c.Writer.Status()/100 != 2 {
-			log.V(2).Info("REST Request failed", values...)
-		} else {
-			log.V(3).Info("REST Request OK", values...)
-		}
-	}()
+		log.V(3).Info("REST Request", values...)
+
+		// Log before sending response
+		defer func() {
+			values := append(common, "status", c.Writer.Status(), "latency", time.Since(start))
+			if len(c.Errors.Errors()) > 0 {
+				values = append(values, "errors", c.Errors.Errors())
+			}
+			if detail {
+				values = append(values, "response", rw.String())
+			}
+			log.V(3).Info("REST Response", values...)
+		}()
+	}
 	c.Next()
 }
